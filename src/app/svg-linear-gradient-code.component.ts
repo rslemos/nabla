b4e05e2d1cf99ee5edbe52a61a28f67dc00d6e25
R. Lemos <rslemos@gmail.com>
2020-08-30 22:18:27 -0300

    parse code to parameters

--- src/app/svg-linear-gradient-code.component.ts
+++ src/app/svg-linear-gradient-code.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { of } from 'rxjs';
+
+import { catchError } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 import { SvgParameters } from './svg-linear-gradient-parameters.component';
 import { SvgModel } from './svg-model.class';
@@ -11,7 +22,7 @@ import { SvgModel } from './svg-model.class';
   templateUrl: './svg-linear-gradient-code.component.html',
   styleUrls: ['./svg-linear-gradient-code.component.scss'],
 })
-export class SvgLinearGradientCodeComponent {
+export class SvgLinearGradientCodeComponent implements OnInit {
   // tslint:disable-next-line: variable-name
   _code = '';
   // tslint:disable-next-line: variable-name
@@ -23,6 +34,33 @@ export class SvgLinearGradientCodeComponent {
   @Input() parameters: SvgParameters;
   @Output() parametersChange = new EventEmitter<SvgParameters>();
 
+  private codeSubject = new Subject<string>();
+
   error = { class: 'empty', message: '' };
 
+  ngOnInit(): void {
+    this.codeSubject.pipe(
+      tap(() => this.error = { class: 'info', message: 'Waiting for input...'}),
+      debounceTime(2000),
+      switchMap(x => of(x).pipe(
+        tap(() => this.error = { class: 'info', message: 'Compiling...'}),
+        map(code => new SvgModel(code)),
+        tap(model => this._model = model),
+        map(model => model?.parameters),
+        tap(() => this.error = { class: 'info', message: 'Done'}),
+        catchError(err => {
+          this.error = { class: 'error', message: err };
+          return of(null as SvgParameters);
+        }),
+        filter(parameters => !!parameters),
+      )),
+    ).subscribe(parameters => {
+      this.linearGradientChange.emit(this._model.linearGradient.cloneNode(true) as Element);
+      this.parametersChange.emit(parameters);
+    });
+  }
+
+  onCodeChange(code: string): void {
+    this.codeSubject.next(code);
+  }
 }
--- src/app/svg-model.class.ts
+++ src/app/svg-model.class.ts
@@ -1,2 +1,145 @@
+import { coerceNumberProperty } from '@angular/cdk/coercion';
+
+import { isColorError } from './color.function';
+import { parseAlphaColor } from './color.function';
+import { parsePlainColor } from './color.function';
+import { regExp } from './color.function';
+
+import { SvgParameters } from './svg-linear-gradient-parameters.component';
+import { SvgStop } from './svg-linear-gradient-parameters.component';
+
+const parser = new DOMParser();
+const serializer = new XMLSerializer();
+
 export class SvgModel {
+  private readonly doc: XMLDocument;
+
+  constructor(code: string) {
+    this.doc = parser.parseFromString(code, 'image/svg+xml');
+  }
+
+  get linearGradient(): Element {
+    const linearGradients = Array.prototype.slice.call(this.doc.getElementsByTagName('linearGradient')) as Element[];
+    if (linearGradients.length === 0) {
+      throw new Error('no linearGradient element found');
+    }
+
+    if (linearGradients.length > 1) {
+      throw new Error('more than one linearGradient element found');
+    }
+
+    return linearGradients[0];
+  }
+
+  // tslint:disable: variable-name
+  get parameters(): SvgParameters {
+    function parseValue(_value: string): number | 'invalid coordinate' {
+      const value = coerceNumberProperty(_value.replace(/%$/, ''), 'invalid coordinate');
+      if (value === 'invalid coordinate') {
+        return value;
+      }
+
+      return /%$/.test(_value)
+        ? value / 100
+        : value;
+    }
+
+    const linearGradient = this.linearGradient;
+
+    const x1 = parseValue(linearGradient.getAttribute('x1') || '0');
+    const y1 = parseValue(linearGradient.getAttribute('y1') || '0');
+    const x2 = parseValue(linearGradient.getAttribute('x2') || '1');
+    const y2 = parseValue(linearGradient.getAttribute('y2') || '0');
+
+    const gradientUnits = linearGradient.getAttribute('gradientUnits') || 'objectBoundingBox';
+
+    if (gradientUnits && ['objectBoundingBox', 'userSpaceOnUse'].indexOf(gradientUnits) < 0) {
+      throw new Error('gradientUnits has invalid value');
+    }
+
+    const stops = (Array.prototype.slice.call(linearGradient.children) as Element[])
+      .map(_stop => {
+        if (_stop.tagName !== 'stop') {
+          throw new Error('only <stop>');
+        }
+
+        const _offset = _stop.getAttribute('offset') || '0';
+        const _stopColor = _stop.getAttribute('stop-color') || 'black';
+        const _stopOpacity = _stop.getAttribute('stop-opacity') || '1';
+
+        return { _offset, _stopColor, _stopOpacity };
+      })
+      .map(_stop => {
+        const offset = parseValue(_stop._offset);
+        if (offset === 'invalid coordinate') {
+          return 'invalid coordinate';
+        }
+
+        const opacity = _stop._stopOpacity !== undefined
+          ? parseFloat(_stop._stopOpacity)
+          : undefined;
+
+        const [
+          // tslint:disable: variable-name
+          /* ignore group 0 (full match) */,
+          _coloralpha,
+          _transparent,
+          _hexcoloralpha,
+          _rgbaabs, _rgbaabs_red, _rgbaabs_green, _rgbaabs_blue, _rgbaabs_alpha,
+          _rgbaperc, _rgbaperc_red, _rgbaperc_green, _rgbaperc_blue, _rgbaperc_alpha,
+          _color,
+          _hexcolor,
+          _rgbabs, _rgbabs_red, _rgbabs_green, _rgbabs_blue,
+          _rgbperc, _rgbperc_red, _rgbperc_green, _rgbperc_blue,
+          _colorkeyword,
+                  // tslint:enable: variable-name
+        ] = regExp.exec(_stop._stopColor);
+
+        if (_coloralpha) {
+          const color = parseAlphaColor([
+            _coloralpha,
+            _transparent,
+            _hexcoloralpha,
+            _rgbaabs, _rgbaabs_red, _rgbaabs_green, _rgbaabs_blue, _rgbaabs_alpha,
+            _rgbaperc, _rgbaperc_red, _rgbaperc_green, _rgbaperc_blue, _rgbaperc_alpha,
+          ]);
+          if (isColorError(color)) {
+            return color;
+          }
+
+          return opacity !== undefined
+            ? { offset, color, opacity } as SvgStop
+            : { offset, color } as SvgStop;
+        }
+
+        if (_color) {
+          const color = parsePlainColor([
+            _color,
+            _hexcolor,
+            _rgbabs, _rgbabs_red, _rgbabs_green, _rgbabs_blue,
+            _rgbperc, _rgbperc_red, _rgbperc_green, _rgbperc_blue,
+            _colorkeyword,
+          ]);
+          if (isColorError(color)) {
+            return color;
+          }
+
+          return opacity !== undefined
+            ? { offset, color, opacity } as SvgStop
+            : { offset, color } as SvgStop;
+        }
+      });
+
+    const firstInvalidStop = stops.findIndex(stop => typeof stop === 'string');
+    if (firstInvalidStop >= 0) {
+      throw new Error(`Line ${firstInvalidStop}: ${stops[firstInvalidStop]}`);
+    }
+
+    return { x1, y1, x2, y2, gradientUnits, stops } as SvgParameters;
+  }
+  // tslint:enable: variable-name
+
+  public toString(): string {
+    return serializer.serializeToString(this.doc).replace(/></g, '>\n<');
+  }
 }
